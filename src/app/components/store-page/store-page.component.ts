b0c5cdb6a329ea1503e14e4f4151622bf89bb4ed
Adrian Albu <2412rock@gmail.com>
2025-02-19 11:44:16 +0200

    add remaning tiles

--- src/app/components/store-page/store-page.component.ts
+++ src/app/components/store-page/store-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { PurchaseItemTileComponent } from '../purchase-item-tile/purchase-item-tile.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-store-page',
   standalone: true,
-  imports: [PurchaseItemTileComponent],
+  imports: [PurchaseItemTileComponent, CommonModule],
   templateUrl: './store-page.component.html',
   styleUrl: './store-page.component.scss'
 })
