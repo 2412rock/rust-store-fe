016c6ecea0362b1077370bf194372fed8b682d16
Adrian Albu <2412rock@gmail.com>
2025-02-19 16:01:46 +0200

    obj

--- obj/Debug/net8.0/Rust store backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/Rust store backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rust store backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63530eebf0bf770c510d0c927de022e7bb13c593")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rust store backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rust store backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
